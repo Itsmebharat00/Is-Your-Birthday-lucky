877929ebe39b459535307a519ab055c163076cfd
bharats9768 <74733419+bharats9768@users.noreply.github.com>
2021-09-23 14:47:24 +0530

    return in outputbox

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum,luckyNumber) {
+    if (sum%luckyNumber===0) {
+        outputBox.innertext ="Your birthday is lucky";
+    } else {
+        outputBox.innertext ="Your birthday is not lucky";
+    }
+}
+
+// function compareValues(sum,luckyNumber){
+//     if(sum%luckyNumber===0){
+//       outputBox.innerText="Your birthday is lucky 🚀";
+//     }else {
+//       outputBox.innerText="Your birthday is not lucky 🤒";
+//     }
+//   }
+
+function checkBirthdayIsLucky() {
+    const dob = dateOfBirth.value
+    const sum = calculateSum(dob);
+    compareValues(sum, luckyNumber.value)
+
+}
 
 
-checkNumberButton.addEventListener("click", function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
 
-})
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+
+}
+
+
+checkNumberButton.addEventListener("click", checkBirthdayIsLucky)
+
